2f61f07ae4cc313ccb0bb769b0563c3e9d39a0b6
stefansujdovic <clickattackcacak@gmail.com>
2017-12-15 13:59:01 +0100

    Video promenee

---
+++ App.js
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import Video from 'react-native-video';
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+            <Video
+            repeat
+            resizeMode='cover'
+            source={require('./Video/Proba.mp4')}
+            style={styles.backgroundVideo}
+          />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+});
